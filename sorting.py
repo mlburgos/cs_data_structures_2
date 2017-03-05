aecef6b634f6eb416e37c7e4d7a54d53b394898f
Monica Burgos <mlburgos12@gmail.com>
2017-03-05 23:06:21 +0000

    Week 9 assessment

---
+++ sorting.py
@@ -0,0 +1,90 @@
+#Sorting
+
+
+def bubble_sort(lst):
+    """Returns a sorted list using a optimized bubble sort algorithm
+    i.e. using a variable to track if there hasn't been a swap.
+
+        >>> bubble_sort([3, 5, 7, 2, 4, 1])
+        [1, 2, 3, 4, 5, 7]
+    """
+
+    for j in range(len(lst)):
+        swap = False
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                swap = True
+
+        if not swap:
+            break
+
+    return lst
+
+
+def merge_lists(list1, list2):
+    """Given two sorted lists of integers, returns a single sorted list
+    containing all integers in the input lists.
+
+    >>> merge_lists([1, 3, 9], [4, 7, 11])
+    [1, 3, 4, 7, 9, 11]
+    """
+
+    new_list = []
+
+    while list1 or list2:
+        pass
+        if list1 == []:
+            new_list.extend(list2)
+            list2 = []
+        elif list2 == []:
+            new_list.extend(list1)
+            list1 = []
+        elif list1[0] < list2[0]:
+            new_list.append(list1[0])
+            list1 = list1[1:]
+        else:
+            new_list.append(list2[0])
+            list2 = list2[1:]
+
+    return new_list
+
+
+##########ADVANCED##########
+def merge_sort(lst):
+    """
+    Given a list, returns a sorted version of that list.
+
+    Finish the merge sort algorithm by writing another function that takes in a
+    single unsorted list of integers and uses recursion and the 'merge_lists'
+    function you already wrote to return a new sorted list containing all
+    integers from the input list. In other words, the new function should sort
+    a list using merge_lists and recursion.
+
+    >>> merge_sort([6, 2, 3, 9, 0, 1])
+    [0, 1, 2, 3, 6, 9]
+    """
+
+    if len(lst) < 2:
+        return lst
+
+    middle = int(len(lst)/2)
+    list1 = merge_sort(lst[:middle])
+    list2 = merge_sort(lst[middle:])
+
+    return merge_lists(list1, list2)
+
+
+
+
+#####################################################################
+# END OF ASSIGNMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
